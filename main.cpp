1e213c3907dc9184fa85240eba6b485ee5f1a143
Ryan Chau <118787511+yum1234@users.noreply.github.com>
2024-03-24 18:49:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
-#include "../include/main.h"
+#include "main.h"
 #include <memory>
 using namespace std;
 
+//opcontrol declerations
 void arcade(std::shared_ptr<ChassisController> drive, Controller &controller);
 void flywheelHandling(ControllerButton &flywheelSpinIn, ControllerButton &flywheelSpinOut, Motor &flywheel);
+void intakeHandling(ControllerButton &intakeIn, ControllerButton &intakeOut, Motor &intake);
+void wingHandling(ControllerButton &wingsIn, ControllerButton &wingsOut, Motor &leftWing, Motor &rightWing);
+void testAutoInitiate(ControllerButton &testAuto);
+
+//autonomous declarations
+void openWings(std::shared_ptr<AsyncPositionController<double, double>> leftWing, std::shared_ptr<AsyncPositionController<double, double>> rightWing);
+void closeWings(std::shared_ptr<AsyncPositionController<double, double>> leftWing, std::shared_ptr<AsyncPositionController<double, double>> rightWing);
+void waitWings(std::shared_ptr<AsyncPositionController<double, double>> leftWing, std::shared_ptr<AsyncPositionController<double, double>> rightWing);
+
+struct {
+	//chassis
+	int topLeftMotor;
+	int topRightMotor;
+	int bottomLeftMotor;
+	int bottomRightMotor;
+	//general motor
+	int intake;
+	int flywheel;
+	int leftWing;
+	int rightWing;
+	//sensors
+	int gps;
+
+
+} ports;
 
 /**
  * A callback function for LLEMU's center button.
@@ -28,10 +54,31 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
+	//setting struct
+
+	//general
+	ports.intake = 5;
+	ports.flywheel = 6;
+	ports.leftWing = 7;
+	ports.rightWing = 8;
+
+	//chassis
+	ports.topLeftMotor = 1;
+	ports.topRightMotor = 2;
+	ports.bottomRightMotor = 3;
+	ports.bottomLeftMotor = 4;
+
+	//sensors
+	ports.gps = 9;
+
+	//for lcd screen
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello Ryan and Nathan!");
-
 	pros::lcd::register_btn1_cb(on_center_button);
+
+
+
+
 }
 
 /**
@@ -63,7 +110,54 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	//Resources
+		//https://www.skillsusa.org/wp-content/uploads/2023/11/2024-SkillsUSA-MRT-Game-Manual-S-PS.pdf
+		//https://www.vexrobotics.com/over-under-manual
+		//https://okapilib.github.io/OkapiLib/md_docs_tutorials_walkthrough_asyncAutonomousMovement.html
+		//https://okapilib.github.io/OkapiLib/md_docs_tutorials_walkthrough_odometry.html
+
+	//safety switch:
+	//ControllerButton testAuto(ControllerDigital::X);
+
+	//Chassis decleration
+	std::shared_ptr<OdomChassisController> drive = ChassisControllerBuilder()
+			.withMotors(ports.topLeftMotor, ports.topRightMotor, ports.bottomRightMotor, ports.bottomLeftMotor)
+			// Green gearset, 4 in wheel diam, 11.5 in wheel track
+			.withDimensions(AbstractMotor::gearset::green*(60.0/36.0), {{4_in, 11.751_in}, imev5GreenTPR})
+			.withOdometry(StateMode::FRAME_TRANSFORMATION)
+			.buildOdometry();
+
+	//async motor declerations
+		//intake
+	std::shared_ptr<AsyncPositionController<double, double>> intake = AsyncPosControllerBuilder()
+    	.withMotor(ports.intake) // lift motor port 3
+    	.build();
+		//flywheel
+	std::shared_ptr<AsyncPositionController<double, double>> flywheel = AsyncPosControllerBuilder()
+		.withMotor(ports.flywheel)
+		.build();
+		//right wing
+	std::shared_ptr<AsyncPositionController<double, double>> rightWing = AsyncPosControllerBuilder()
+		.withMotor(ports.rightWing)
+		.build();
+		//left wing
+	std::shared_ptr<AsyncPositionController<double, double>> leftWing = AsyncPosControllerBuilder()
+		.withMotor(ports.leftWing)
+		.build();
+
+	//sensors declerations
+		//GPS
+		pros::GPS gps(ports.gps);
+		pros::c::gps_status_s_t gpsData; 
+
+	//main process
+	drive->moveDistance(30_in);
+	openWings(leftWing, rightWing);
+	waitWings(leftWing, rightWing);
+	drive->moveDistance(20_in);
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -81,32 +175,48 @@ void autonomous() {}
 void opcontrol() {
 	//Controller declerations
 	Controller controller;
-	ControllerButton flywheelSpinIn(ControllerDigital::R1);
-	ControllerButton flywheelSpinOut(ControllerDigital::R2);
+		//for flywhel
+	ControllerButton flywheelSpinIn(ControllerDigital::A);
+	ControllerButton flywheelSpinOut(ControllerDigital::B);
+		//for intake
+	ControllerButton intakeIn(ControllerDigital::R1);
+	ControllerButton intakeOut(ControllerDigital::R2);
+		//for wings
+	ControllerButton wingsIn(ControllerDigital::L1);
+	ControllerButton wingsOut(ControllerDigital::L2);
+		//for testing autonomous control
+	ControllerButton testAuto(ControllerDigital::X);
+
 
 	//motor declerations
-	Motor intake(5);
-	Motor flywheel(6);
-	Motor leftwing(7);
-	Motor rightwing(8);
+	Motor intake(ports.intake);
+	Motor flywheel(ports.flywheel);
+	Motor leftWing(ports.leftWing);
+	Motor rightWing(ports.rightWing);
 
 	//motor configuration declerations
-	flywheel.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
-	leftwing.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
-	rightwing.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+	flywheel.setBrakeMode(okapi::AbstractMotor::brakeMode::coast); //https://www.vexforum.com/t/strange-okapi-xdrivemodel-setbrakemode-behavior/95937
+	intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+	leftWing.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+	rightWing.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
 
 	//Chassis decleration
 	std::shared_ptr<ChassisController> drive = ChassisControllerBuilder()
-			.withMotors(1, 2, 3, 4)
+			.withMotors(ports.topLeftMotor, ports.topRightMotor, ports.bottomRightMotor, ports.bottomLeftMotor)
 			// Green gearset, 4 in wheel diam, 11.5 in wheel track
 			.withDimensions(AbstractMotor::gearset::green*(60.0/36.0), {{4_in, 11.751_in}, imev5GreenTPR})
 			.build();
 
+	//main loop
 	while (true) {
 		arcade(drive, controller);
 		flywheelHandling(flywheelSpinIn, flywheelSpinOut, flywheel); 
+		intakeHandling(intakeIn, intakeOut, intake);
+		wingHandling(wingsIn, wingsOut, leftWing, rightWing);
 
+		testAutoInitiate(testAuto);
 
+		//give scheduler time to do other tasks
 		pros::delay(10);
 	}
 
@@ -116,6 +226,8 @@ void opcontrol() {
 }
 
 
+
+//functions for op control
 void arcade(std::shared_ptr<ChassisController> drive, Controller &controller) {
 	//handles driving
 	drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::leftX));
@@ -125,15 +237,89 @@ void arcade(std::shared_ptr<ChassisController> drive, Controller &controller) {
 void flywheelHandling(ControllerButton &flywheelSpinIn, ControllerButton &flywheelSpinOut, Motor &flywheel) {
 	//handles buttons and motors for the flywheel
 	if(flywheelSpinIn.isPressed()) {
-		flywheel.moveVelocity(1400);
+		flywheel.moveVelocity(200);
 	}
 
 	else if(flywheelSpinOut.isPressed()) {
-		flywheel.moveVelocity(-1400);
+		flywheel.moveVelocity(-200);
 	}
 
 	else {
 		flywheel.moveVelocity(0);
 	}
+
+}
+
+void intakeHandling(ControllerButton &intakeIn, ControllerButton &intakeOut, Motor &intake) {
+	//handles buttons and motors for the intake
+	if(intakeIn.isPressed()) {
+		intake.moveVelocity(200);
+	}
+
+	else if(intakeOut.isPressed()) {
+		intake.moveVelocity(-200);
+	}
+
+	else {
+		intake.moveVelocity(0);
+	}
+}
+
+void wingHandling(ControllerButton &wingsIn, ControllerButton &wingsOut, Motor &leftWing, Motor &rightWing) {
+	//handles buttons and motors for the wings.
+	if(wingsIn.isPressed()) {
+		leftWing.moveVelocity(200);
+		rightWing.moveVelocity(200);
+	}
+
+	if(wingsOut.isPressed()) {
+		leftWing.moveVelocity(-200);
+		rightWing.moveVelocity(-200);
+	}
+}
+
+void testAutoInitiate(ControllerButton &testAuto) {
+	if(testAuto.isPressed()) {
+		autonomous();
+	}
+}
+
+//functions for autonomous
+void openWings(std::shared_ptr<AsyncPositionController<double, double>> leftWing, std::shared_ptr<AsyncPositionController<double, double>> rightWing) {
+	//opens both wings
+	leftWing->setTarget(90);
+	rightWing->setTarget(90);
+}
+
+void closeWings(std::shared_ptr<AsyncPositionController<double, double>> leftWing, std::shared_ptr<AsyncPositionController<double, double>> rightWing) {
+	//closes both wings
+	leftWing->setTarget(-90);
+	rightWing->setTarget(-90);
+}
+
+void waitWings(std::shared_ptr<AsyncPositionController<double, double>> leftWing, std::shared_ptr<AsyncPositionController<double, double>> rightWing) {
+	//waits for the wings to finish their targets
+	leftWing->waitUntilSettled();
+	rightWing->waitUntilSettled();
+}
+
+void moveCood(pros::Gps &gps, std::shared_ptr<OdomChassisController> drive, double targetX, double targetY, double targetAngle) {
+	//this function calculates the difference between the current gps point, and the target gps point (place you want to go on the vex coodinate system).
+	//Then it goes to use that difference and add it to the odometry, and sets that as the point to move towards.
+	//Inches only!!!
+	//https://www.vexforum.com/t/using-okapi-units/83933 convert double to qlength or some other units.
+	pros::c::gps_status_s_t gpsData;
+	gpsData = gps.get_status();
+
+	QLength diffX = (targetX-gpsData.x)*1.0_m; //convert meters to inches
+	QLength diffY = -(targetY-gpsData.y)*1.0_m; //negative is reqiured due to the different coodinate systems
+	QAngle diffAngle = (90.0+(targetAngle-gps.get_heading()))*1_deg;
+
+	QLength relativeX = drive->getState().x;
+	QLength relativeY = drive->getState().y;
+	QAngle relativeAngle = drive->getState().theta;
+
+	drive->driveToPoint({relativeX + diffX, relativeY + diffY});
+	drive->turnToAngle(relativeAngle + diffAngle);
 
 }
